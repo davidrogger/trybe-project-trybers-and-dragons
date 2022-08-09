32117b81a20bacf0b6129ed829de422fc4f65bfa
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-09 17:00:01 -0300

    feat: create class Ranger and Warrior
    new classes Ranger and Warrior

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,24 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Warrior extends Archetype {
+  private static _createdArchetypeInstances = 0;
+  private _energyType: EnergyType;
+  constructor(
+    name: string,
+  ) {
+    super(name);
+    Warrior._createdArchetypeInstances += 1;
+    this._energyType = 'stamina';
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances() {
+    return Warrior._createdArchetypeInstances;
+  }
+}
+
+export default Warrior;
