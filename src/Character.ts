9efab79c1571d540daf48e14b6d483b932df58ee
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-09 17:36:12 -0300

    feat: add attributes to the new class
    add all attributos to the Character classes

---
+++ src/Character.ts
@@ -0,0 +1,34 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+
+class Character implements Fighter {
+  private _race: Race;
+  private _archtype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(
+    private _name: string,
+  ) {
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(this._name, this._dexterity);
+    this._archtype = new Mage(this._name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = {
+      type_: this._archtype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
+}
+
+export default Character;
